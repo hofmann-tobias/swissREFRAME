683d946097adb0f4ceb32a491beeea8e493e9c42
adal02 <hofmann.tobias121@gmail.com>
2019-11-02 00:47:47 +0100

    V1.0.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/adal02/swissREFRAME',
-    # download_url='https://github.com/adal02/PyREFRAME/archive/V1.0.0.dev0.tar.gz',
+    download_url='https://github.com/adal02/swissREFRAME/archive/V1.0.0.tar.gz',
     install_requires=['comtypes'],
     packages=setuptools.find_packages(),
     include_package_data=True,
