d7d39680ef270bbef9dfbd7e75e254920ec26b15
adal02 <hofmann.tobias121@gmail.com>
2019-11-02 00:11:26 +0100

    V1.0.0

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='pyreframe',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pyREFRAME-adal02',
     version='1.0.0.dev0',
-    packages=['reframe'],
-    url='https://github.com/adal02/PyREFRAME',
-    license='MIT',
-    author='Tobias Hofmann',
+    author='adal02',
     author_email='hofmann.tobias121@gmail.com',
-    description='Interface for the official swisstopo\'s REFRAME DLL'
+    description="Interface for the official swisstopo's REFRAME DLL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/adal02/PyREFRAME',
+    download_url='https://github.com/adal02/PyREFRAME/archive/V1.0.0.dev0.tar.gz',
+    install_requires=['comtypes'],
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    keywords=["geodesy", "geography", "swiss", "coordinates", "transformation"],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3',
+    ]
 )
