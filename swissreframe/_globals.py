8e26842d523379ae4356e4f79f2cfaab561d1a4e
adal02 <hofmann.tobias121@gmail.com>
2019-11-06 23:16:19 +0100

    clean up

--- swissreframe/_globals.py
+++
@@ -1,6 +0,0 @@
-
-
-list_plan_frames = ['lv03', 'lv95']
-list_alti_frames = ['ln02', 'lhn95', 'bessel1841']
-
-r = None
--- swissreframe/_utils.py
+++
@@ -1,5 +0,0 @@
-import math
-
-
-def deg2gon(angle_deg):
-    return angle_deg / 180 * 200
